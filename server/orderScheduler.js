c7ff222e4717da4dd9eb20a958494edb691ac400
Sherwin Fernandes <sherwfernand@gmail.com>
2021-09-14 16:36:32 +0530

    Fixed option to register as cook/user

--- server/orderScheduler.js
+++ server/orderScheduler.js
@@ -4,9 +4,9 @@ module.exports=function (orders){
     for(order in orders){
         const time=order.deliveryDate.getHours()*3600 +order.deliveryDate.getMinutes()*60+ order.deliveryDate.getDays()*3600*24
         var j = schedule.scheduleJob('*/'+ time+'* * * * *', function(){
-            const order = await Order.findById(req.params.id)
+            const order = Order.findById(req.params.id)
             order.status = "placed"
-            const updatedOrder = await order.save()
+            const updatedOrder = order.save()
           });
     }
     
